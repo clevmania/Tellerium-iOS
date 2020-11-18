eb8bbb06b35fea890e3441151842d5191068b136
clevmania <ernestcleverest@yahoo.com>
2020-11-18 15:50:27 +0100

    fix: font style not taking effect on simulator

--- tellerium-iOS/Controllers/LoginViewController.swift
+++ tellerium-iOS/Controllers/LoginViewController.swift
@@ -12,6 +12,7 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var signInButton: UIButton!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var dontHaveAnAccountLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,9 +21,23 @@ class LoginViewController: UIViewController {
         Utility.styleTextField(emailTextField)
         Utility.styleTextField(passwordTextField)
         Utility.stylePrimaryButton(signInButton)
+        styleNoAccountLabel()
     }
 
     @IBAction func signInButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    func styleNoAccountLabel(){
+        if let font = UIFont(name: "SourceSansPro-Regular", size: 14), let boldFont = UIFont.init(name: "SourceSansPro-SemiBold", size: 14) {
+           let attributedNoAccountText = NSMutableAttributedString(string: "Don't have an account? ", attributes: [NSAttributedString.Key.font : font])
+
+            let attributedSignUpText = NSMutableAttributedString(string: "Sign Up", attributes: [NSAttributedString.Key.font : boldFont])
+
+            attributedNoAccountText.append(attributedSignUpText)
+            dontHaveAnAccountLabel.attributedText = attributedNoAccountText
+        }
+        
         
     }
 }
