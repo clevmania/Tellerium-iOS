07ffa0b15c77dd69d0990b1c4aaa6b147dd57bb9
clevmania <ernestcleverest@yahoo.com>
2020-11-21 21:36:50 +0100

    feat: register new users

--- tellerium-iOS/Controllers/RegisterViewController.swift
+++ tellerium-iOS/Controllers/RegisterViewController.swift
@@ -7,16 +7,33 @@
 //
 
 import UIKit
+import Firebase
 
 class RegisterViewController: UIViewController {
 
+    @IBOutlet weak var etEmailTextField: UITextField!
+    @IBOutlet weak var etPasswordTextField: UITextField!
+    @IBOutlet weak var etLastNameTextField: UITextField!
+    @IBOutlet weak var etFirstNameTextField: UITextField!
+    @IBOutlet weak var etMobileTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func registerUserButton(_ sender: Any) {
+        if let email = etEmailTextField.text, let password = etPasswordTextField.text {
+            Auth.auth().createUser(withEmail: email, password: password) { (authResult, error) in
+                if let err = error {
+                    print(err.localizedDescription)
+                } else {
+                    self.performSegue(withIdentifier: "RegisterIdentifier", sender:self)
+                }
+            }
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
